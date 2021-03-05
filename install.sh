cb57060b73d19d5f827b78dba982dbda2d431f4c
shisuichan01 <79549704+shisuichan01@users.noreply.github.com>
2021-03-05 08:53:20 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+pkg install git
+pkg install wget
+git clone https://github.com/pmmp/PocketMine-MP
+cd PoketMine-MP
+mkdir -p bin/php7/bin
+cd bin/php7/bin
+wget 
+http://download831.mediafire.com/wj84uwaceh0g/ptq8rtx0aj4s3ca/php
+chmod +X php
+cd
+cd PoketMine-MP
+wget https://jenkins.pmmp.io/job/PocketMine-MP/lastSuccessfulBuild/artifact/PocketMine-MP.phar
